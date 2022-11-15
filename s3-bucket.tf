c07c1e70a65c0d8e98e43bb638fbab66dd6a879e
Ivan Sierra <ivansierradelrio14@gmail.com>
2022-11-15 11:40:13 +0000

    solve conflict

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-bucket_prefix = "ivansierra"
-  bucket = "my-hashicorp-bucket"
+  bucket_prefix = "ivansierra"
   acl    = "private"
 
   versioning = {
